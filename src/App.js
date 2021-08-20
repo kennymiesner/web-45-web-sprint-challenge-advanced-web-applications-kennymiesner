a77ead09910546d2d77f81799c34e2dcdc96ad7e
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-20 11:37:40 -0500

    Adds login functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axiosWithAuth from "./helpers/axiosWithAuth";
 
-import Login from "./components/Login";
+import PrivateRoute from './components/PrivateRoute'
+import Login from './components/Login'
+import BubblePage from './components/BubblePage'
 import "./styles.scss";
 
 function App() {
+  const handleLogout = () => {
+    axiosWithAuth()
+      .post('/logout')
+      .then(() => {
+        localStorage.removeItem('token')
+        window.location.href = '/login'
+      })
+  }
+
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
+          <a onClick={handleLogout} data-testid="logoutButton" href="#">logout</a>
         </header>
+        <Switch>
+          <PrivateRoute path='/bubbles' component={BubblePage}/>
+          <Route path='/login' component={Login} />
+          <Route exact path='/' component={Login} />
+        </Switch>
       </div>
     </Router>
   );
@@ -19,7 +36,9 @@ function App() {
 
 export default App;
 
-//Task List:
-//1. Add in two routes that link to the Login Component, one for the default path '/' and one for the '/login'.
-//2. Render BubblePage as a PrivateRoute
-//2. Build the logout button to call the logout endpoint, remove the localStorage Item and redirect to the login page.
+/*
+  Task List:
+  1. Add in two routes that link to the Login Component, one for the default path '/' and one for the '/login'.
+  2. Render BubblePage as a PrivateRoute
+  2. Build the logout button to call the logout endpoint, remove the localStorage Item and redirect to the login page.
+*/
